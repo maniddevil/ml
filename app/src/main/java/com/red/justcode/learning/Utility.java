f4b1e8e7461ec2d40e55255b426fc4bb29bb5637
maniddevil <mmanidhar@gmail.com>
2017-11-05 01:44:26 +0530

    State class with empty methods added

--- app/src/main/java/com/red/justcode/learning/Utility.java
+++ app/src/main/java/com/red/justcode/learning/Utility.java
@@ -1,8 +1,38 @@
 package com.red.justcode.learning;
 
+import android.content.Context;
+
+import java.util.List;
+
 /**
  * Created by manidhar on 11/4/2017.
  */
 
 public class Utility {
+
+    //test this
+    public static class State {
+        int state[][] = new int[3][3];
+        public State(int[] stateArray) {
+            for(int i=0; i<state.length; i++) {
+                int length = state[i].length;
+                for(int j=0; j<length; j++) {
+                    state[i][j] = stateArray[i*length + j];
+                }
+            }
+        }
+
+        public float getOFloatState() {
+            return 0;
+        }
+        public float getXFloatState() {
+            return 0;
+        }
+    }
+
+    public void addStatesToLookupTable(Context context, List<Integer[]> stateList, boolean hasWon) {
+        final boolean isWon = hasWon;
+        final boolean isDraw = !hasWon;
+
+    }
 }
